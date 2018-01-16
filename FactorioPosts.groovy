e286a5b074a5ce58497ce597ef1591302eabbc17
Duga Jenkins <zomisforsberg@gmail.com>
2018-01-16 20:51:16 +0100

    Add groovy script using my Duga bot, currently just write to sysout

---
+++ FactorioPosts.groovy
@@ -0,0 +1,133 @@
+#!/usr/bin/env groovy
+
+@GrabResolver(name='zomis', root='http://www.zomis.net/maven')
+@Grab(group='net.zomis', module='duga-core', version='0.2')
+import net.zomis.duga.chat.BotConfiguration
+import net.zomis.duga.chat.StackExchangeChatBot
+import net.zomis.duga.chat.TestBot
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+class FactorioPosts {
+
+def duga = new TestBot()
+def room = duga.room("16134")
+
+def slurpJson(json) {
+  return new JsonSlurper().parseText(json)
+}
+
+def inform(def mod, String title, String body) {
+    def modUrl = "https://mods.factorio.com/mods/$mod.owner/$mod.name"
+    String firstLine = String.format("**\\[[%s](%s)\\]** **%s**", mod.owner + "/" + mod.name, modUrl, title);
+    body = body.split('\n')[0]
+    String secondLine = "> $body"
+    room.message(firstLine).post()
+    room.message(secondLine).post()
+}
+
+def updateMod(known, def mod, def msgs) {
+    def size = msgs.results.size()
+    String key = mod.owner + '/' + mod.name
+    if (!known.containsKey(key)) {
+        println "updateMod: Known did not contain key $key"
+        known[key] = size
+    }
+    println "updateMod: Comparing for $key: ${known[key]} vs. size $size"
+    if (known[key] != size) {
+        int lastKnown = known[key]
+        for (int i = lastKnown; i < size; i++) {
+            println "Inform about msgs $i for $mod.name"
+            inform(mod, msgs.results[i].title, msgs.results[i].message)
+        }
+        known[key] = size
+    }
+}
+
+def updateReplies(known, def mod, def msg, def replies) {
+    def size = replies.results.size()
+    def key = mod.name + '/' + msg.id
+    if (!known.containsKey(key)) {
+        println "updateReplies: Known did not contain key $key"
+        known[key] = size
+    }
+    println "updateReplies: Comparing for $key: ${known[key]} vs. size $size"
+    if (known[key] != size) {
+        int lastKnown = known[key]
+        String modKey = mod.owner + '/' + mod.name
+        for (int i = lastKnown; i < size; i++) {
+            inform(mod, "Re: " + msg.title, replies.results[i].message)
+        }
+        known[key] = size
+    }
+}
+
+def perform(String user, known) {
+    println "Fetching data for $user"
+    def urlData = "https://mods.factorio.com/api/mods?owner=$user&page_size=100&page=1".toURL().text
+    def data = slurpJson(urlData)
+//    println "Fetched: $data"
+//    println "Fetched data for $user"
+    data.results.each { mod ->
+        def modName = mod.name
+        println "Fetching data for $user / $modName"
+        def msgUrlData = "https://mods.factorio.com/api/messages?page_size=50&mod=$modName&page=1&order=oldest".toURL().text
+        def msgs = slurpJson(msgUrlData)
+//        println "Fetched: $msgs"
+        updateMod(known, mod, msgs)
+        msgs.results.each { msg ->
+            def createdAt = msg.created_at
+            def lastReplyAt = msg.last_reply_at
+            def header = msg.title
+            def body = msg.message
+            
+            println "Fetching data for $user / $modName / $header"
+            def repliesUrlData = "https://mods.factorio.com/api/messages?page_size=100&order=oldest&parent=$msg.id&page=1".toURL().text
+            def replies = slurpJson(repliesUrlData)
+//            println "Fetched: $replies"
+            updateReplies(known, mod, msg, replies)
+        }
+    }
+}
+
+def saveResult(String dataFile, def known) {
+    try {
+    def builder = new JsonBuilder()
+    builder(known)
+    println builder.toString()
+    def file = new File(dataFile)
+    file.write(builder.toString())
+    } catch (Exception ex) {
+        println "Failing: $ex"
+        ex.printStackTrace()
+    }
+}
+
+def runForUser(String user, String dataFile) {
+    println "Run $user with dataFile $dataFile"
+    def known = [:]
+    def file = new File(dataFile)
+    if (file.exists()) {
+        println "Datafile exists"
+        def json = file.text
+        known = slurpJson(json)
+        println "Known read from file: $known"
+        perform(user, known)
+        println "Done with perform."
+        println "Known is after perform: $known"
+    } else {
+        println "Datafile does not exists"
+        perform(user, known)
+        println "Done with perform."
+        println "Known is: $known"
+        room.message('Data file did not exist. Existing threads is ' + known).post()
+    }
+//    saveResult(dataFile, known)
+}
+
+    static void main(String[] args) {
+        new FactorioPosts().runForUser('zomis', 'volume/factorio_posts.json')
+    }
+
+}
+
